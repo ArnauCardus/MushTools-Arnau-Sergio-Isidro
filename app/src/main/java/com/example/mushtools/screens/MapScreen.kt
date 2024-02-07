343ff5452c64ce2919c04ecc95bc418988ef9726
Isidro <Isidro@gmail.com>
2024-02-07 19:54:47 +0100

    Subiendo el maps

--- app/src/main/java/com/example/mushtools/screens/MapScreen.kt
+++ app/src/main/java/com/example/mushtools/screens/MapScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.mushtools.funciones.funciones_maps.MapsScreen
 
 @Composable
 fun Map() {
@@ -12,6 +11,6 @@ fun Map() {
         modifier = Modifier.fillMaxSize(),
     ) {
 
-        MapsScreen()
+        //MapsScreen()
     }
 }
