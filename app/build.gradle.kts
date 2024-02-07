e3cd0f40ae2fbec694b634c3ebe06d5d77b44c43
Sergio Jimenez <145470975+SergioJP1@users.noreply.github.com>
2024-02-07 19:58:36 +0100

    Conexion firebase 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-analytics")
-
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -61,6 +60,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/mushtools/screens/QuizScreen.kt
+++ app/src/main/java/com/example/mushtools/screens/QuizScreen.kt
@@ -1,17 +1,39 @@
 package com.example.mushtools.screens
 
+import android.content.ContentValues
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.mushtools.models.Items_Setas
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlin.math.log
 
 @Composable
 fun Quiz(){
+    val db = FirebaseFirestore.getInstance()
     Column (
         modifier = Modifier
             .fillMaxSize(),
     ) {
         Text(text = "Quiz")
+
+        val Setaslista : MutableList<Items_Setas> = mutableListOf<Items_Setas>()
+
+
+
+        db.collection("Setas").get() .addOnSuccessListener { result ->
+                for (document in result) {
+                    val seta : Items_Setas = document.toObject(Items_Setas::class.java)
+                    Setaslista.add(seta)
+                    Log.d("Setas","$Setaslista")
+                    //Log.d(ContentValues.TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "Error getting documents: ", exception)
+            }
     }
 }
