f85a220499dcb0887ec98b7d716c4813810caf66
Isidro <Isidro@gmail.com>
2024-02-29 18:31:35 +0100

    Log Out funcional

--- app/src/main/java/com/example/mushtools/components/PantallaPrincipal.kt
+++ app/src/main/java/com/example/mushtools/components/PantallaPrincipal.kt
@@ -1,12 +1,12 @@
 package com.example.mushtools.components
 
+import TopBar
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/example/mushtools/components/TopBar.kt
+++ app/src/main/java/com/example/mushtools/components/TopBar.kt
@@ -1,6 +1,9 @@
-package com.example.mushtools.components
 
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ExitToApp
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerState
@@ -9,7 +12,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import com.example.mushtools.screens.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,6 +25,8 @@ fun TopBar(
     drawerState: DrawerState
 ){
     val scope = rememberCoroutineScope()
+    val firebaseAuth = FirebaseAuth.getInstance()
+
     CenterAlignedTopAppBar(
         title = { Text(text = "MushTool")},
         navigationIcon = {
@@ -25,10 +34,34 @@ fun TopBar(
                 scope.launch{
                     drawerState.open()
                 }
-
             }) {
-                Icon(Icons.Outlined.Menu,"Abrir Menu")
+                Icon(Icons.Outlined.Menu,"Open Menu")
+            }
+        },
+        actions = {
+            // Botón de log out
+            LogoutButton {
+                firebaseAuth.signOut()
             }
         }
-        )
+    )
 }
+private val LocalContext = compositionLocalOf<Context> { error("No Context provided") }
+
+@Composable
+private fun RowScope.LogoutButton(onClick: () -> Unit) {
+    val context = LocalContext.current
+    IconButton(
+        onClick = {
+            onClick()
+            // Redirigir a la pantalla de inicio de sesión después de cerrar sesión
+            val intent = Intent(context, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            context.startActivity(intent)
+        }
+    ) {
+        Icon(Icons.AutoMirrored.Outlined.ExitToApp, contentDescription = "Log out")
+    }
+}
+
+
